b4f105cd8ed2840e03909c3ac1b2d1c273a6aef2
Solmar <solmaruboldi@gmail.com>
2025-04-22 17:10:17 -0300

    Update CreateTaskButton and TaskSearch

--- src/CreateTaskButton.js
+++ src/CreateTaskButton.js
@@ -2,7 +2,15 @@ import './CreateTaskButton.css';
 
 function CreateTaskButton() {
     return (
-        <button className="btn btn-primary">New Task</button>
+        <button className="btn btn-primary"
+            onClick={
+                (event) => {
+                    alert('Create a new task!')
+                    console.log(event);
+                    console.log(event.target);
+                }
+            }
+        >New Task</button>
     );
 }
 
--- src/TaskSearch.js
+++ src/TaskSearch.js
@@ -1,9 +1,22 @@
+import React from 'react';
 import './TaskSearch.css';
 
 function TaskSearch() {
+
+    const [event, setEvent] = React.useState('');
+
+    console.log("Los ususarios buscan: " + event);
+
     return (
         <div>
-            <input type="text" placeholder="Buscar tarea" />
+            <input 
+                type="text" 
+                placeholder="Buscar tarea"
+                value={event}
+                onChange={(event) => {
+                    setEvent(event.target.value);
+                }}
+            />
         </div>
     );
 }
