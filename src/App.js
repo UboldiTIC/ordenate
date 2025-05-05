4e042650b8805317cfa5fabef8134fb3a6680a47
Tuta <solmaruboldi@gmail.com>
2025-05-04 23:04:52 -0300

    Add localStorage as persistence

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import { TaskItem } from './TaskItem';
 import { CreateTaskButton } from './CreateTaskButton';
 import './App.css';
 
-const defaultTasks = [
+/* const defaultTasks = [
   { text: 'Completar el curso.', completed: true },
   { text: 'Realizar el proyecto.', completed: false },
   { text: 'Rendir el examen.', completed: false },
   { text: 'Festejar.', completed: false },
   { text: 'Averiguar fecha.', completed: true },
 ];
+ */
+//localStorage.setItem('TASK_V1', JSON.stringify(defaultTasks));
+
+// localStorage.removeItem('TASK_V1');
 
 function App() {
 
@@ -21,7 +25,27 @@ function App() {
   // cada objeto tiene una propiedad text y completed
   // el valor de la propiedad text es un string
   // el valor de la propiedad completed es un booleano.
-  const [tasks, setTasks] = React.useState(defaultTasks);
+  
+  // const [tasks, setTasks] = React.useState(defaultTasks);
+
+
+  // CREAMOS UN ESTADO para las tareas a partir de useState usando localStorage.
+
+  const localStorageTasks = localStorage.getItem('TASK_V1');
+  let parsedTasks;
+
+  if (!localStorageTasks) {
+    localStorage.setItem('TASK_V1', JSON.stringify([]));
+    parsedTasks = [];
+  }
+  else {
+    parsedTasks = JSON.parse(localStorageTasks);
+  }
+  // Guardamos el valor de localStorage en el estado tasks.
+
+ 
+  const [tasks, setTasks] = React.useState(parsedTasks);
+  //const [searchValue, setSearchValue] = React.useState('');
 
   // CREAMOS UN ESTADO para el evento a partir de useState
   // el valor inicial del estado es un string vacío
@@ -30,15 +54,23 @@ function App() {
   // es importante tener en cuenta que este estado va a actualizarse a partir del evento onChange.
   // el evento onChange va a recibir un evento como parámetro.
   const [event, setEvent] = React.useState('');
-
   console.log("Los ususarios buscan: " + event);
+
+  // CRUD: vamos a actualizar el estado y localStorage con la siguiente función:
+
+  const saveTasks = (newTasks) => {
+    localStorage.setItem('TASK_V1', JSON.stringify(newTasks));
+
+    setTasks(newTasks);
+  }
+
 
   // Función para completar una tarea
   const completeTask = (text) => {
     const newTasks = [...tasks];
     const taskIndex = tasks.findIndex(task => task.text === text);
     newTasks[taskIndex].completed = !newTasks[taskIndex].completed;
-    setTasks(newTasks);
+    saveTasks(newTasks);
   };
 
   // Función para eliminar una tarea
@@ -46,7 +78,7 @@ function App() {
     const newTasks = [...tasks];
     const taskIndex = tasks.findIndex(task => task.text === text);
     newTasks.splice(taskIndex, 1);
-    setTasks(newTasks);
+    saveTasks(newTasks);
   };
 
   // Estados Derivados: a partir de estados ya existentes, nos permiten realizar cálculos.
