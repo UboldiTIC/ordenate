c076107bebd2fb40534b37d6a8af25ae50f26b0d
Solmar <solmaruboldi@gmail.com>
2025-08-11 11:15:30 -0300

    Update TaskCounter

--- src/TaskCounter/index.js
+++ src/TaskCounter/index.js
@@ -16,7 +16,7 @@ function TaskCounter() {
                 Tareas completadas: {completedTasks} de {totalTasks}.
             </h2>
             {completedPercentage === 100 ? (
-                <h3>Felicitaciones. No tienes tareas pendientes</h3>
+                <h3>No tienes tareas pendientes.</h3>
             ) : (
                 <h3>
                     Has completado el {completedPercentage}% de {totalTasks} tareas.
